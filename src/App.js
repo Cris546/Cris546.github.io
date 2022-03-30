94a7d4901f28f854be5fa1906d12f088df01e02f
Cristobal Cortez <theonlyjeff223@gmail.com>
2022-03-29 21:23:28 -0700

    Started work on landing page

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
           <Route path='/portfolio' element={<Portfolio/>} />
           <Route path='/contact' element={<Contact/>} />
         </Switch>
+        
       </Router>
       
       
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import '../styles/Home.css'
+
+import pic from "../assests/cris.jpg"
+
 function Home() {
   return (
-    <div>Hello world!</div>
+    <div className='intro'>
+      <div className='left'>
+        <div className='imgContainer'>
+          <img src={pic} alt='cat' />
+        </div>
+      </div>
+      <div className='right'>right</div>
+    </div>
   )
 }
 
