ef0871c453c271d81f43c31228043b634e42279e
Cristobal Cortez <theonlyjeff223@gmail.com>
2022-04-11 18:07:51 -0700

    More work on landing page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,26 @@ function Home() {
           <img src={pic} alt='cat' />
         </div>
       </div>
-      <div className='right'>right</div>
+      <div className='right'>
+        <div className='right-wrapper'>
+          <h2 className='right-intro'>The name's</h2>
+          <h1 className='right-name'>Cristobal Cortez</h1>
+          <div className='right-title'>
+            <div className='right-title-wrapper'>
+              <div className='right-title-item'>Backend Developer</div>
+              <div className='right-title-item'>Database Designer</div>
+              <div className='right-title-item'>Frontend Developer</div>
+              <div className='right-title-item'>Guitar Player</div>
+              <div className='right-title-item'>Full Stack Engineer</div>
+
+            </div>
+          </div>
+          <div className='right-desc'>
+            Words Words Words Words Words Words Words Words Words Words Words Words
+            
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
